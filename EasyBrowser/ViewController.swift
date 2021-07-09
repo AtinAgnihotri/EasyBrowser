46677c2233a6bdf13bdf78c07d33765597601f87
AtinAgnihotri <atinagnihotri@gmail.com>
2021-07-09 20:23:13 +0530

    Adds domain safety to ViewController

--- EasyBrowser/ViewController.swift
+++ EasyBrowser/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
     var spacerBarButtonItem: UIBarButtonItem!
     var refreshBarButtonItem: UIBarButtonItem!
     
+    let websites = ["atinagnihotri.com", "hackingwithswift.com", "apple.com", "google.com"]
+    
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
@@ -30,7 +32,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
     }
     
     func loadUrlToWebView() {
-        let url = URL(string: "https://atinagnihotri.com")!
+        let url = URL(string: "https://" +  websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
@@ -85,9 +87,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page . . .", message: nil, preferredStyle: .actionSheet)
-        let sites = ["atinagnihotri.com", "hackingwithswift.com", "apple.com", "google.com"]
-        for eachSite in sites {
-            ac.addAction(UIAlertAction(title: eachSite, style: .default, handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
         }
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
@@ -102,6 +103,22 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        
+        decisionHandler(.cancel)
     }
 
 }
